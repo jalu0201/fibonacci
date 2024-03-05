55fa766043c98583028a5f9b612379afa104a350
Jacob Lundgren <jacob.lundgren@skola.sala.se>
2024-03-05 14:46:28 +0100

    gjorde fibonacci med for loop

--- fibonacci/Form1.cs
+++ fibonacci/Form1.cs
@@ -9,7 +9,18 @@ namespace fibonacci
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int tal1 = 1;
+            int tal2 = 0;
+            int tal3 = 0;
 
+
+            for (int i = 0; i <= 30; i++)
+            {
+                tal3 = tal1 + tal2;
+                tal1 = tal2;
+                tal2 = tal3;
+                textBox1.Text = textBox1.Text + " " + tal3.ToString();
+            }
         }
     }
 }
